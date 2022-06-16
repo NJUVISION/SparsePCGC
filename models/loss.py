b95ef98f7d5d7dfc5f11e225622805f99f935b0e
Jianqiang Wang <1139970854@qq.com>
2022-06-16 19:30:30 +0800

    Add files via upload

---
+++ models/loss.py
@@ -0,0 +1,46 @@
+import os, sys
+rootdir = os.path.split(__file__)[0]
+sys.path.append(rootdir)
+rootdir = os.path.split(rootdir)[0]
+sys.path.append(rootdir)
+import torch
+import numpy as np
+from collections import Counter
+from data_utils.sparse_tensor import isin, istopk
+criterion = torch.nn.BCEWithLogitsLoss()
+
+
+def get_bce(data, groud_truth):
+    """ Input data and ground_truth are sparse tensor.
+    """
+    mask = isin(data.C, groud_truth.C)
+    bce = criterion(data.F.squeeze(), mask.type(data.F.dtype))
+    bce /= torch.log(torch.tensor(2.0)).to(bce.device)
+    sum_bce = bce * data.shape[0]
+    
+    return sum_bce
+
+def get_bits(likelihood):
+    bits = -torch.sum(torch.log2(likelihood))
+
+    return bits
+
+def get_metrics(data, groud_truth):
+    mask_real = isin(data.C, groud_truth.C)
+    nums = [len(C) for C in groud_truth.decomposed_coordinates]
+    mask_pred = istopk(data, nums, rho=1.0)
+    metrics = get_cls_metrics(mask_pred, mask_real)
+
+    return metrics[0]
+
+def get_cls_metrics(pred, real):
+    TP = (pred * real).cpu().nonzero(as_tuple=False).shape[0]
+    FN = (~pred * real).cpu().nonzero(as_tuple=False).shape[0]
+    FP = (pred * ~real).cpu().nonzero(as_tuple=False).shape[0]
+    TN = (~pred * ~real).cpu().nonzero(as_tuple=False).shape[0]
+
+    precision = TP / (TP + FP + 1e-7)
+    recall = TP / (TP + FN + 1e-7)
+    IoU = TP / (TP + FP + FN + 1e-7)
+
+    return [round(precision, 4), round(recall, 4), round(IoU, 4)]
